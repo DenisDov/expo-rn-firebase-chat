2418508dc1e59e3f05fb01fb3e09d0e8fdd960e6
DenisDov <dovzdenis@gmail.com>
2023-11-10 16:17:20 +0200

    base gifted chat

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ### RN Firebase chat with expo-dev-client
 
-- Firestore db for gifted chat
 - Email and password auth
+- Firestore db for gifted chat
+
+### Create native folders after added package with plugin or added new ggogle-services.json from firebase to root folder
+
+`npx expo prebuild --platform ios --clean`
+`npx expo prebuild --platform android --clean`
--- src/screens/protected/chat.tsx
+++ src/screens/protected/chat.tsx
@@ -1,9 +1,58 @@
-import { Box, Text } from '@app/theme';
+import firestore from '@react-native-firebase/firestore';
+import { useCallback, useEffect, useState } from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+
+import { useAuth } from '@app/context/auth';
+
+const chatsCollection = firestore().collection('Chats');
 
 export const ChatScreen = () => {
+  const { user } = useAuth();
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = chatsCollection.onSnapshot(q => {
+      const messages: any = [];
+
+      q.forEach(doc => {
+        console.log('doc: ', doc.data());
+        messages.push({
+          _id: doc.data()._id,
+          text: doc.data().text,
+          createdAt: doc.data().createdAt,
+          user: doc.data().user,
+        });
+      });
+
+      setMessages(messages);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+
+    const { _id, createdAt, text, user } = messages[0];
+    chatsCollection.add({
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
+
   return (
-    <Box>
-      <Text>CHAT H</Text>
-    </Box>
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={false}
+      showUserAvatar={false}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: user?.uid,
+      }}
+    />
   );
 };
